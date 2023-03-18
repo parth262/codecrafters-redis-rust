8daafee8068a1ea9789611a727580d198b1bb6cb
Parth Pandya <parthrpandya98@gmail.com>
2023-03-18 11:49:13 +0530

    Handles multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, BufWriter, Write};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,16 +12,33 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                match stream.write_all("+PONG\r\n".as_bytes()) {
-                    Ok(_) => {println!("success")}
-                    Err(e) => {println!("Error {}", e)}
-                }
-            }
+            Ok(stream) => {
+                handle_connection(stream);
+            },
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+
+    drop(listener)
+}
+
+fn handle_connection(stream: TcpStream) {
+    println!("Connection opened");
+    loop {
+        let mut message = String::new();
+        let mut reader = BufReader::new(&stream);
+        if let Ok(size) = reader.read_line(&mut message) {
+            if size == 0 {
+                println!("Connection closed");
+                break;
+            }
+            println!("Server received: {} | Message size: {}", message, size);
+
+            let mut writer = BufWriter::new(&stream);
+            writer.write_all("+PONG\r\n".as_bytes()).expect("unable to write");
+            writer.flush().expect("could not flush");
         }
     }
 }
